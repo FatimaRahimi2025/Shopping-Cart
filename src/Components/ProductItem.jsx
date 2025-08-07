ac11dfe5ede9ecc29b9a9914ef026d68ca2063e8
Fatima Rahimi <rahimifatima432@gmail.com>
2025-08-07 10:31:06 +0430

    create tow Button for increase and decrease

--- src/Components/ProductItem.jsx
+++ src/Components/ProductItem.jsx
@@ -1,15 +1,34 @@
 import { useContext } from 'react';
-import {Card, Button} from 'react-bootstrap'
+import {Card, Button, Form, Row, Col} from 'react-bootstrap'
 import { CartContext } from '../Context/CartContext';
 function ProductItem({product}){
    const cart = useContext(CartContext)
+   const productQuantity = cart.getProductQuantity(product.id)
 return(
     <Card className="mt-5 card-bg">
         <Card.Body>
             <Card.Img variant="top" src={product.image} height="200px" style={{ObjectFit:'cover'}}/>
             <Card.Title align='right' className="text-light pt-4">{product.title}</Card.Title>
-            <Card.Text align="right" className="text-light">{product.price}</Card.Text>
-            <Button onClick={() => cart.addItemToCart(product.id)} variant='btn  btn-outline-secondary' className='text-white'>افزودن به سبد خرید</Button>
+            <Card.Text align="right" className="text-light" dir='rtl'>{product.price}تومان</Card.Text>
+            {productQuantity > 0 ? (
+                <>
+                <Form as={Row}>
+                    <Form.Label column="true" sm="6" className="text-white">
+                        تعداد: {productQuantity}
+                    </Form.Label>
+                    <Col sm='6'>
+                    <Button sm='6' className='mx-2 text-white ' variant='btn btn-outline-secondary'
+                    onClick={() => cart.addItemToCart(product.id)}>+</Button>
+                    <Button sm="6" className='mx-2 text-white' variant='btn btn-outline-secondary'
+                    onClick={() => cart.removeItemFromCart(product.id)}>-</Button>
+                    </Col>
+                </Form>
+                </>
+            ): (
+                <Button onClick={() => cart.addItemToCart(product.id)} variant='btn  btn-outline-secondary' className='text-white'>افزودن به سبد خرید</Button>
+            )
+        }
+            
         </Card.Body>
     </Card>
 )
--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -22,7 +22,7 @@ export function CartProvider({ children }) {
     return quantity
     }
 
-    function addItemToCart(){
+    function addItemToCart(id){
         const quantity = getProductQuantity(id)
 
         if(quantity === 0) {
